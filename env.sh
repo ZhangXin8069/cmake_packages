65612e2a0dc4548078ea3c0ac50116d5dec0996b
zhangxin <zhangxin8069@qq.com>
2023-08-04 17:26:16 +0800

    2023-08-04-17-26-16

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # install_path=$(cd ${file_path}/../external-libraries && pwd) #absolute path
-install_path=$(cd ~/external-libraries && pwd) #absolute path
+install_path=${HOME}/external-libraries #absolute path
 mkdir ${install_path} -p
